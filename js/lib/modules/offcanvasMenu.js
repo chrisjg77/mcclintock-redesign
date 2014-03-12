d796c2eb26c752da474a773e71e3980a96c29033
Chris Griffith <chrisg@terraeclipse.com>
2014-03-12 18:00:31 -0400

    Adds check to mobile nav toggle if window is resized to desktop

--- js/lib/modules/offcanvasMenu.js
+++ js/lib/modules/offcanvasMenu.js
@@ -1,17 +1,23 @@
-
 define(['jquery'],function( $ ) {
 
-	$(document).on('click','#menuHandle',function() {
+	// Shorten jquery selectors
+	var body = $('body');
 
-		var body = $('body');
+	// On menu icon click, open the menu if it is not already open
+	$(document).on('click','#menuHandle',function() {
 		if ( !body.hasClass('menuOpen') ) {
 			body.addClass('menuOpen')
 		} 
 		else {
 			body.removeClass('menuOpen');
 		}
-		
-
 	});
-
+	// After the menu is open, check for window resizing.
+	// If window is resized greater than desktop width, close the nav
+	// menu.
+	$(window).on('resize',function() {
+		if ( $(this).width() > 1024 ) {
+			body.removeClass('menuOpen');
+		}
+	});
 });
